656bee76e99b9405db039412af005a7c035f924c
Gholts <104642670+Gholts@users.noreply.github.com>
2025-08-23 04:42:44 +0800

    change nvim colorscheme

--- .config/nvim/lua/gholts/lazy/bufferline.lua
+++ .config/nvim/lua/gholts/lazy/bufferline.lua
@@ -5,28 +5,40 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    vim.cmd.colorscheme("edge")
+    vim.cmd.colorscheme("oxocarbon")
     vim.opt.termguicolors = true
     vim.o.mousemoveevent = true
     require('bufferline').setup {
-        options = {
-            indicator = {
-                syyle = 'underline'
-            },
-            offsets = {
-                {
-                    filetype = "NvimTree",
-                    text = "File Explorer",
-                    text_align = "left",
-                    separator = true
-                }
-            },
-            hover = {
-                enabled = true,
-                delay = 200,
-                reveal = {'close'}
-            }
+      options = {
+        indicator = {
+            style = 'underline'
         },
+
+        max_name_length = 14,
+        max_prefix_length = 13,
+        tab_size = 10,
+        show_tab_indicators = true,
+        enforce_regular_tabs = false,
+        show_buffer_close_icons = true,
+        show_close_icon = false,
+        separator_style = "thick",
+        always_show_bufferline = true,
+
+        hover = {
+            enabled = true,
+            delay = 200,
+            reveal = {'close'}
+        },
+
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "File Explorer",
+                text_align = "left",
+                separator = true
+            }
+        }
+      },
     }
   end
 }
--- .config/nvim/lua/gholts/lazy/colorscheme.lua
+++ .config/nvim/lua/gholts/lazy/colorscheme.lua
@@ -1,14 +1,11 @@
 return {
 
   {
-    'sainnhe/edge',
+    'nyoom-engineering/oxocarbon.nvim',
     lazy = false,
     priority = 1000,
     config = function()
-      vim.g.edge_better_performance = 1
-      vim.g.edge_transparent_background = 1
-      vim.g.edge_enable_italic = true
-      vim.cmd.colorscheme('edge')
+      vim.cmd.colorscheme('oxocarbon')
 
    end,
  },
