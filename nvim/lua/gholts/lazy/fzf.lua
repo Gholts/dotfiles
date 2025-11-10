92f278fb5ab9bbfb91708585be0603f071d054c9
Hugo Lee <104642670+Gholts@users.noreply.github.com>
2025-11-10 15:53:54 +0800

    feat: add configuration

--- nvim/lua/gholts/lazy/fzf.lua
+++ nvim/lua/gholts/lazy/fzf.lua
@@ -3,13 +3,62 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("fzf-lua").setup({
-			winopts = {},
-			keymap = {},
-			actions = {},
-			fzf_opts = {},
-			fzf_colors = {},
-			hls = {},
-			previewers = {},
+			winopts = {
+				split = "belowright new", -- open in a split instead?
+				-- "belowright new"  : split below
+				-- "aboveleft new"   : split above
+				-- "belowright vnew" : split right
+				-- "aboveleft vnew   : split left
+				-- border argument passthrough to nvim_open_win()
+				border = "rounded",
+				fullscreen = false, -- start fullscreen?
+				treesitter = {
+					enabled = true,
+					fzf_colors = { ["hl"] = "-1:reverse", ["hl+"] = "-1:reverse" },
+				},
+				preview = {
+					default = "bat", -- override the default previewer?
+					border = "rounded", -- preview border: accepts both `nvim_open_win`
+					-- and fzf values (e.g. "border-top", "none")
+					-- native fzf previewers (bat/cat/git/etc)
+					-- can also be set to `fun(winopts, metadata)`
+					wrap = false, -- preview line wrap (fzf's 'wrap|nowrap')
+					hidden = false, -- start preview hidden
+					vertical = "down:50%", -- up|down:size
+					horizontal = "right:50%", -- right|left:size
+					layout = "flex", -- horizontal|vertical|flex
+					flip_columns = 100, -- #cols to switch to horizontal on flex
+					-- Only used with the builtin previewer:
+					title = false, -- preview border title (file/buf)?
+					title_pos = "center", -- left|center|right, title alignment
+					scrollbar = "float", -- `false` or string:'float|border'
+					-- float:  in-window floating border
+					-- border: in-border "block" marker
+					scrolloff = -1, -- float scrollbar offset from right
+					-- applies only when scrollbar = 'float'
+					delay = 20, -- delay(ms) displaying the preview
+				},
+			},
+			previewers = {
+				bat = {
+					cmd = "bat",
+					args = "--color=always --style=numbers,changes",
+				},
+				git_diff = {
+					-- if required, use `{file}` for argument positioning
+					-- e.g. `cmd_modified = "git diff --color HEAD {file} | cut -c -30"`
+					cmd_deleted = "git diff --color HEAD --",
+					cmd_modified = "git diff --color HEAD",
+					cmd_untracked = "git diff --color --no-index /dev/null",
+					-- git-delta is automatically detected as pager, set `pager=false`
+					-- to disable, can also be set under 'git.status.preview_pager'
+				},
+				man = {
+					-- NOTE: remove the `-c` flag when using man-db
+					-- replace with `man -P cat %s | col -bx` on OSX
+					cmd = "man -P bat %s | col -bx",
+				},
+			},
 		})
 	end,
 }
