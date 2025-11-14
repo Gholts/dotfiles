e587071bbb861ba65812396e892e4641dc2e539c
Hugo Lee <104642670+Gholts@users.noreply.github.com>
2025-11-15 05:41:04 +0800

    chore: format keymapping

--- nvim/lua/gholts/lazy/misc.lua
+++ nvim/lua/gholts/lazy/misc.lua
@@ -27,11 +27,12 @@ return {
 	},
 	{
 		"wansmer/treesj",
-		keys = { "<space>m" },
 		dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
 		config = function()
 			require("treesj").setup({--[[ your config ]]
+				use_default_keymaps = false,
 			})
+			vim.keymap.set("n", "<leader>m", require("treesj").toggle)
 		end,
 	},
 	{
