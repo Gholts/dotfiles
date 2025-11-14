e555ed22f2601d70f88f6302b77391845fdb4fcb
Hugo Lee <104642670+Gholts@users.noreply.github.com>
2025-11-15 05:40:22 +0800

    fix: fix fillchars border color

--- nvim/lua/gholts/lazy/colorscheme.lua
+++ nvim/lua/gholts/lazy/colorscheme.lua
@@ -5,5 +5,12 @@ return {
 	config = function()
 		vim.cmd.colorscheme("oxocarbon")
 		vim.opt.termguicolors = true
+
+		local colors = require("oxocarbon").oxocarbon
+
+		if colors then
+			vim.api.nvim_set_hl(0, "FloatBorder", { fg = colors.base03, bg = colors.blend })
+			vim.api.nvim_set_hl(0, "FloatBorder", { fg = colors.base03, bg = "none" })
+		end
 	end,
 }
