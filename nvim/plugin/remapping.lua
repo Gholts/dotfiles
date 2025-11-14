eac2a87d4806cf54a249289b9260fe6a8890612b
Hugo Lee <104642670+Gholts@users.noreply.github.com>
2025-11-15 05:46:21 +0800

    feat, style, del
    feat: add oil keymap
    feat: add undo tree keymap
    feat: make quit map quit all
    feat: add souce map
    style: using local map instead of local bind
    del: remove NvimTree keymap

--- nvim/plugin/remapping.lua
+++ nvim/plugin/remapping.lua
@@ -1,38 +1,37 @@
 -------------------------------------------------------no-whichkey
-local bind = vim.keymap.set
+local map = vim.keymap.set
 local bo, cmd, fn, api = vim.bo, vim.cmd, vim.fn, vim.api
 local ic = { "i", "c" }
 local mapOption = { noremap = true, silent = true }
 ------------------------------------------------------------------
 -- Native
 ------------------------------------------------------------------
+-- Source configs
+map("n", "<leader>ro", ":so<CR>", mapOption)
 -- Save
-bind("n", "<leader>s", "<cmd>w<cr>", mapOption)
+map("n", "<leader>s", "<cmd>w<cr>", mapOption)
 -- Quit
-bind("n", "<leader>qq", "<cmd>q<cr>", mapOption)
+map("n", "<leader>qq", "<cmd>qa<cr>", mapOption)
 -- Force Quit
-bind("n", "<Bslash>q", ":q!<CR>", mapOption)
+map("n", "<Bslash>q", ":q!<CR>", mapOption)
 ------------------------------------------------------------------
 -- Buffer
 ------------------------------------------------------------------
--- Cycle Switch Buffer by Buffer Line
-bind("n", "<C-p>", "<cmd>BufferLineCycleNext<cr>", mapOption)
-bind("n", "<C-n>", "<cmd>BufferLineCyclePrev<cr>", mapOption)
 -- Go to Buffer
-bind("n", "<localleader>1", "<cmd>BufferLineGoToBuffer 1<cr>", mapOption)
-bind("n", "<localleader>2", "<cmd>BufferLineGoToBuffer 2<cr>", mapOption)
-bind("n", "<localleader>3", "<cmd>BufferLineGoToBuffer 3<cr>", mapOption)
-bind("n", "<localleader>4", "<cmd>BufferLineGoToBuffer 4<cr>", mapOption)
-bind("n", "<localleader>5", "<cmd>BufferLineGoToBuffer 5<cr>", mapOption)
-bind("n", "<localleader>6", "<cmd>BufferLineGoToBuffer 6<cr>", mapOption)
-bind("n", "<localleader>7", "<cmd>BufferLineGoToBuffer 7<cr>", mapOption)
-bind("n", "<localleader>8", "<cmd>BufferLineGoToBuffer 8<cr>", mapOption)
-bind("n", "<localleader>9", "<cmd>BufferLineGoToBuffer 9<cr>", mapOption)
+map("n", "<localleader>1", "<cmd>BufferLineGoToBuffer 1<cr>", mapOption)
+map("n", "<localleader>2", "<cmd>BufferLineGoToBuffer 2<cr>", mapOption)
+map("n", "<localleader>3", "<cmd>BufferLineGoToBuffer 3<cr>", mapOption)
+map("n", "<localleader>4", "<cmd>BufferLineGoToBuffer 4<cr>", mapOption)
+map("n", "<localleader>5", "<cmd>BufferLineGoToBuffer 5<cr>", mapOption)
+map("n", "<localleader>6", "<cmd>BufferLineGoToBuffer 6<cr>", mapOption)
+map("n", "<localleader>7", "<cmd>BufferLineGoToBuffer 7<cr>", mapOption)
+map("n", "<localleader>8", "<cmd>BufferLineGoToBuffer 8<cr>", mapOption)
+map("n", "<localleader>9", "<cmd>BufferLineGoToBuffer 9<cr>", mapOption)
 ------------------------------------------------------------------
 -- Windows
 ------------------------------------------------------------------
 -- Close Current Buffer
-bind("n", "<leader>w", function()
+map("n", "<leader>w", function()
 	local function smart_close()
 		if bo.filetype == "NvimTree" then
 			cmd.NvimTreeClose()
@@ -43,25 +42,31 @@ bind("n", "<leader>w", function()
 	smart_close()
 end, mapOption)
 -- Fuzzy File Search fzf-lua
-bind("n", "<C-/>", "<cmd>FzfLua files<cr>", mapOption)
--- Toggle NvimTree
-bind("n", "<D-C-c>", "<cmd>NvimTreeToggle<cr>", mapOption)
+map("n", "<C-/>", "<cmd>FzfLua files<cr>", mapOption)
+-- Toggle Oil float
+map("n", "<D-C-c>", function()
+	require("oil").toggle_float()
+end, mapOption)
+-- Open Oil
+map("n", "-", "<cmd>Oil<cr>", mapOption)
+-- Toggle Undotree
+map("n", "<D-C-v>", "<cmd>UndotreeToggle<cr>", mapOption)
 -- Switch to Next Window
-bind("n", "<leader><Tab>", "<C-w>w", mapOption)
+map("n", "<leader><Tab>", "<C-w>w", mapOption)
 ------------------------------------------------------------------
 -- Navigation
 ------------------------------------------------------------------
-bind(ic, "<C-k>", "<Up>", mapOption)
-bind(ic, "<C-j>", "<Down>", mapOption)
-bind(ic, "<C-h>", "<Left>", mapOption)
-bind(ic, "<C-l>", "<Right>", mapOption)
+map(ic, "<C-k>", "<Up>", mapOption)
+map(ic, "<C-j>", "<Down>", mapOption)
+map(ic, "<C-h>", "<Left>", mapOption)
+map(ic, "<C-l>", "<Right>", mapOption)
 -- Goto first/last character of the line
-bind({ "n", "v" }, "gl", "$", mapOption)
-bind({ "n", "v" }, "gh", "^", mapOption)
+map({ "n", "v" }, "gl", "$", mapOption)
+map({ "n", "v" }, "gh", "^", mapOption)
 ------------------------------------------------------------------
 -- Boolean Switch
 ------------------------------------------------------------------
-bind("n", "<leader>t", function()
+map("n", "<leader>t", function()
 	local word = fn.expand("<cword>") -- get current word under cursor
 	if word == "true" then
 		api.nvim_command("normal! ciwfalse") -- make it false
@@ -72,7 +77,7 @@ end, mapOption)
 ------------------------------------------------------------------
 -- Copy Full Text
 ------------------------------------------------------------------
-bind("n", "<C-0>", function()
+map("n", "<C-0>", function()
 	local function indent_restore_cursor()
 		local original_view = fn.winsaveview()
 		cmd("normal! ggVGy")
@@ -83,11 +88,10 @@ end, mapOption)
 ------------------------------------------------------------------
 -- Format
 ------------------------------------------------------------------
-bind("n", "<C-=>", function()
+map("n", "<C-=>", function()
 	local function indent_restore_cursor()
 		local original_view = fn.winsaveview()
 		cmd("normal! ggVG=") -- built-in indent
-		-- vim.lsp.buf.format({ async = false }) -- lsp format
 		fn.winrestview(original_view)
 	end
 	indent_restore_cursor()
