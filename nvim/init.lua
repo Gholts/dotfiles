1b0160ab1a3ca8cce9374e0574972bf1ccaac0cb
Hugo Lee <104642670+Gholts@users.noreply.github.com>
2026-01-13 10:39:11 +0800

    feat: add better autodir

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,10 +11,45 @@
 require("gholts.settings")
 require("gholts.ui")
 ------------------------------------------------------------------
--- Global namespace
+-- Global Namespace
 ------------------------------------------------------------------
 local namespace = {}
 _G.gl = gl or namespace
+------------------------------------------------------------------
+-- Auto Command
+-----------------------------------------------------------autodir
+local function change_cwd_to_arg()
+	vim.schedule(function()
+		local arg = vim.fn.argv(0)
+		if not arg or arg == "" then
+			return
+		end
+
+		if type(arg) == "string" and arg:match("^oil://") then
+			arg = arg:gsub("^oil://", "")
+		end
+
+		if type(arg) == "string" and arg ~= "" then
+			local full_path = vim.fn.fnamemodify(arg, ":p")
+			if vim.fn.isdirectory(full_path) == 1 then
+				vim.cmd.cd(full_path)
+				if vim.fn.getcwd() == full_path:gsub("/$", "") then
+					return
+				else
+					vim.notify("The attempt to switch failed.", vim.log.levels.WARN)
+				end
+			end
+		end
+	end)
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = change_cwd_to_arg,
+})
+
+if vim.v.vim_did_enter == 1 then
+	change_cwd_to_arg()
+end
 ------------------------------------------------------------------
 -- Plugins
 ------------------------------------------------------------------
@@ -32,6 +67,14 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-	spec = "gholts.lazy",
+	spec = {
+		{ import = "gholts.lazy" },
+	},
+	rocks = {
+		hererocks = true,
+	},
 	change_detection = { notify = false },
+	ui = {
+		border = "single",
+	},
 })
