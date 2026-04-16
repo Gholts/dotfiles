1f6f1a05e9a956779cdce8d19c4bc3ea7f883da9
Hugo Lee <104642670+Gholts@users.noreply.github.com>
2026-04-16 15:28:46 +0800

    feat: add trigger of neovim treesitter engine
    Signed-off-by: Hugo Lee <104642670+Gholts@users.noreply.github.com>

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,6 +1,9 @@
--- require("nvim-treesitter").setup {
--- }
-
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "css", "html", "javascript", "typescript", "tsx", "astro" },
+	callback = function()
+		vim.treesitter.start()
+	end,
+})
 require("nvim-treesitter").install({
 	"vimdoc",
 	"javascript",
