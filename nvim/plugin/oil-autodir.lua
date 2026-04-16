b4118ccb974a495bf736f98569e62a8d865b0f31
Hugo Lee <104642670+Gholts@users.noreply.github.com>
2026-04-16 15:27:07 +0800

    move oil-autodir from init.lua to a separate file
    Signed-off-by: Hugo Lee <104642670+Gholts@users.noreply.github.com>

---
+++ nvim/plugin/oil-autodir.lua
@@ -0,0 +1,32 @@
+local function change_cwd_to_arg()
+	vim.schedule(function()
+		local arg = vim.fn.argv(0)
+		if not arg or arg == "" then
+			return
+		end
+
+		if type(arg) == "string" and arg:match("^oil://") then
+			arg = arg:gsub("^oil://", "")
+		end
+
+		if type(arg) == "string" and arg ~= "" then
+			local full_path = vim.fn.fnamemodify(arg, ":p")
+			if vim.fn.isdirectory(full_path) == 1 then
+				vim.cmd.cd(full_path)
+				if vim.fn.getcwd() == full_path:gsub("/$", "") then
+					return
+				else
+					vim.notify("The attempt to switch failed.", vim.log.levels.WARN)
+				end
+			end
+		end
+	end)
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = change_cwd_to_arg,
+})
+
+if vim.v.vim_did_enter == 1 then
+	change_cwd_to_arg()
+end
