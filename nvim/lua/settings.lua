cdd9dab52d11e0ec5c00c4a7fc5ce78f22885d6a
Hugo Lee <104642670+Gholts@users.noreply.github.com>
2026-04-16 13:17:16 +0800

    feat: disable relative line number and native autocomplete
    Signed-off-by: Hugo Lee <104642670+Gholts@users.noreply.github.com>

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,4 +1,3 @@
-------------------------------------------------------------------
 local g, o = vim.g, vim.o
 ------------------------------------------------------------------
 g.mapleader = " " -- remap leader key
@@ -6,7 +5,7 @@ g.maplocalleader = "," -- remap local leader key
 g.netrw_nogx = 1
 ------------------------------------------------------------------
 o.nu = true -- line number
-o.rnu = true -- relative line number
+o.rnu = false -- relative line number
 o.wrap = false -- line warpping
 o.scrolloff = 10 -- add n(10) line offset from the windows edge
 o.splitkeep = "screen" -- make split keep same line
@@ -16,6 +15,7 @@ o.tabstop = 4 -- what tab character occupies looks like
 o.shiftwidth = 4 -- how many space for indent
 o.expandtab = true -- make <Tab> actually type space
 ------------------------------------------------------------------
+o.autocomplete = false -- native autocomplete
 o.clipboard = "unnamedplus" -- make (y),(p) directly work with system clipboard
 o.smartindent = true -- like smart indent
 o.smartcase = true -- like smart auto case insensitive
@@ -23,9 +23,7 @@ o.smoothscroll = true
 o.autochdir = false -- automatically change current dir
 o.undofile = true -- persistent undo
 ------------------------------------------------------------------
--- For smart hidden cmd line default value
+-- Default value for smart hidden of cmdline
 -------------------------------------------------------cmdline.lua
 o.cmdheight = 0
 o.laststatus = 2
-------------------------------------------------------------------
-vim.diagnostic.config({ virtual_text = true })
