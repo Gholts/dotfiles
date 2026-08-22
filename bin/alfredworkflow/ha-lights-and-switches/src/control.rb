2d8c63bdbf957dda7c1db0e9956431340d9160c3
Hugo Lee <dev@proxies.foo>
2026-08-22 19:26:50 +0800

    remove ha-lights-and-switches alfredworkflow
    Signed-off-by: Hugo Lee <dev@proxies.foo>

--- bin/alfredworkflow/ha-lights-and-switches/src/control.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'ha'
-
-home_assistant = HomeAssistant.new
-input = ARGV.join(' ')
-entity_id, brightness = input.split(' ')
-
-puts home_assistant.set_entity_state(entity_id, brightness)
